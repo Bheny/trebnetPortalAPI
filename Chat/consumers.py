4621ebeb408323a19433245147d2fe24658025e6
louxsdon <louisayivi.dev@gmail.com>
2023-03-31 16:20:16 +0000

    update chat consumer

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,8 +1,130 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_id = self.scope['url_route']['kwargs']['room_id']
-        self.room_group_name = f"room_{self.room_id}"
+import json
+from django.shortcuts import get_object_or_404
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.utils.timezone import now
+from django.conf import settings
+from typing import Generator
+from djangochannelsrestframework.generics import GenericAsyncAPIConsumer, AsyncAPIConsumer
+from djangochannelsrestframework.observer.generics import (ObserverModelInstanceMixin, action)
+from djangochannelsrestframework.observer import model_observer
+from djangochannelsrestframework.permissions import IsAuthenticated
 
-        await self.channel_layer.group_add
+from .models import Room, Message
+from .serializers import MessageSerializer, RoomSerializer
+from Profiles.models import Profile as User
+from Profiles.serializers import UserSerializer
+
+class ChatConsumer(ObserverModelInstanceMixin, GenericAsyncAPIConsumer):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "pk"
+    
+
+    async def disconnect(self, code):
+        if hasattr(self, "room_subscribe"):
+            await self.remove_user_from_room(self.room_subscribe)
+            await self.notify_users()
+        await super().disconnect(code)
+
+    @action()
+    async def join_room(self, pk, **kwargs):
+        self.room_subscribe = pk
+        await self.add_user_to_room(pk)
+        await self.notify_users()
+
+    @action()
+    async def leave_room(self, pk, **kwargs):
+        await self.remove_user_from_room(pk)
+
+    @action()
+    async def create_message(self, message, **kwargs):
+        room: Room = await self.get_room(pk=self.room_subscribe)
+        await database_sync_to_async(Message.objects.create)(
+            room=room,
+            user=self.scope["user"],
+            text=message
+        )
+
+    @action()
+    async def subscribe_to_messages_in_room(self, pk, request_id, **kwargs):
+        await self.message_activity.subscribe(room=pk, request_id=request_id)
+
+    @model_observer(Message, serializer_class=MessageSerializer)
+    async def message_activity(
+        self,
+        message,
+        observer=None,
+        subscribing_request_ids = [],
+        **kwargs
+    ):
+        """
+        This is evaluated once for each subscribed consumer.
+        The result of `@message_activity.serializer` is provided here as the message.
+        """
+        # since we provide the request_id when subscribing we can just loop over them here.
+        for request_id in subscribing_request_ids:
+            message_body = dict(request_id=request_id)
+            message_body.update(message)
+            await self.send_json(message_body)
+
+    @message_activity.groups_for_signal
+    def message_activity(self, instance: Message, **kwargs):
+        yield 'room__{instance.room_id}'
+        yield f'pk__{instance.pk}'
+
+    @message_activity.groups_for_consumer
+    def message_activity(self, room=None, **kwargs):
+        if room is not None:
+            yield f'room__{room}'
+
+    # @message_activity.serializer
+    # def message_activity(self, instance:Message, action, **kwargs):
+    #     """
+    #     This is evaluated before the update is sent
+    #     out to all the subscribing consumers.
+    #     """
+    #     return dict(data=MessageSerializer(instance).data, action=action.value, pk=instance.pk)
+
+    async def notify_users(self):
+        room: Room = await self.get_room(self.room_subscribe)
+        for group in self.groups:
+            await self.channel_layer.group_send(group,
+                {
+                    'type':'update_users',
+                    'usuarios':await self.current_users(room)
+                }
+            )
+
+    async def update_users(self, event: dict):
+        await self.send(text_data=json.dumps({'usuarios': event["usuarios"]}))
+
+    @database_sync_to_async
+    def get_room(self, pk: int) -> Room:
+        return Room.objects.get(pk=pk)
+
+    @database_sync_to_async
+    def current_users(self, room: Room):
+        return [UserSerializer(user).data for user in room.users.all()]
+
+    @database_sync_to_async
+    def remove_user_from_room(self, room):
+        user:User = self.scope["user"]
+        user.rooms.remove(room)
+
+    @database_sync_to_async
+    def add_user_to_room(self, pk):
+        user:User = self.scope["user"]
+        print(user)
+        if not user.rooms.filter(pk=self.room_subscribe).exists():
+            user.rooms.add(Room.objects.get(pk=pk))
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_id = self.scope['url_route']['kwargs']['room_id']
+#         self.room_group_name = f"room_{self.room_id}"
+
+#         await self.channel_layer.group_add
