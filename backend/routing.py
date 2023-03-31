d7fffb81dc4684964bb9b3a857e0ab184c111577
louxsdon <louisayivi.dev@gmail.com>
2023-03-31 16:22:55 +0000

    add chat routing

--- backend/routing.py
+++ backend/routing.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
 
 from Notifications import consumers
+from Chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/chat/', ChatConsumer.as_asgi()),
 ]
 
