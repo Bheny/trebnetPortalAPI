46b8e88d5753212c1b4e3c2b89730935e7118d73
louxsdon <louisayivi.dev@gmail.com>
2023-03-31 16:24:49 +0000

    include knox urls

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -2,11 +2,15 @@ from .views import *
 from rest_framework import routers
 from django.urls import path, include 
 from knox import views as knox_views 
-from .views import PasswordRecoveryView, PasswordResetView
+from .views import PasswordRecoveryView, PasswordResetView, LoginView
 
 urlpatterns = [
 	path('register', SignUpAPI.as_view()),
-	path('login', SignInAPI.as_view()),
+	# path('login', SignInAPI.as_view()),
+  	path('login/', LoginView.as_view(), name='knox_login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(),
+         name='knox_logoutall'),
 	path('check_username',UsernameAPI.as_view()),
 	path('send_otp',sendOtpViewSet.as_view()),
 	path('check_otp',checkOtpViewSet.as_view()),
